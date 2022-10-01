a74fe22e61384ec517369bd9bb6d7e168670431f
LeeJaeWon <ghkdwpqndtls@naver.com>
2022-10-02 00:05:31 +0900

    - Ui commit.

--- app/src/main/java/com/jeepchief/dh/view/myinfo/TalismanFragment.kt
+++ app/src/main/java/com/jeepchief/dh/view/myinfo/TalismanFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -54,7 +53,7 @@ class TalismanFragment : Fragment() {
                 }
                 talismanDTO = it
             } catch (e: Exception) {
-                Toast.makeText(requireContext(), "Null..", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(requireContext(), "Null..", Toast.LENGTH_SHORT).show()
             }
         }
 
