af07d2b25461f5d47d3e6e32bdfa14525cfa7ba1
LeeJaeWon <ghkdwpqndtls@naver.com>
2022-10-02 03:14:41 +0900

    - Modified showing style at timeline.

--- app/src/main/java/com/jeepchief/dh/view/timeline/TimelineFragment.kt
+++ app/src/main/java/com/jeepchief/dh/view/timeline/TimelineFragment.kt
@@ -9,16 +9,20 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jeepchief.dh.databinding.FragmentTimelineBinding
+import com.jeepchief.dh.model.rest.dto.TimeLineRows
+import com.jeepchief.dh.util.Log
 import com.jeepchief.dh.view.main.fragment.BaseFragment
-import com.jeepchief.dh.view.timeline.adapter.TimeLineAdapter
+import com.jeepchief.dh.view.timeline.adapter.TimeLineDateAdapter
 import com.jeepchief.dh.viewmodel.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 
 class TimelineFragment : BaseFragment() {
     private var _binding: FragmentTimelineBinding? = null
     private val binding get() = _binding!!
     private val viewModel: MainViewModel by activityViewModels()
     private lateinit var mContext: Context
-
+    private val timeLineMap = hashMapOf<String, MutableList<TimeLineRows>>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -37,10 +41,21 @@ class TimelineFragment : BaseFragment() {
 
         viewModel.run {
             timeLine.observe(requireActivity()) {
+                runBlocking(Dispatchers.Default) {
+                    it.timeline.rows.forEach { row ->
+                        timeLineMap[makeHash(row.date)]?.add(row) ?: run {
+                            timeLineMap.put(makeHash(row.date), mutableListOf(row))
+                        }
+                    }
+                    timeLineMap.keys.forEach {
+                        Log.e("map is ${timeLineMap[it]}")
+                    }
+                }
                 binding.rvTimeline.apply {
                     val manager = LinearLayoutManager(mContext)
                     layoutManager = manager
-                    adapter = TimeLineAdapter(it.timeline.rows)
+//                    adapter = TimeLineAdapter(it.timeline.rows)
+                    adapter = TimeLineDateAdapter(timeLineMap)
                     addItemDecoration(DividerItemDecoration(
                         mContext, manager.orientation
                     ))
@@ -54,4 +69,6 @@ class TimelineFragment : BaseFragment() {
         super.onAttach(context)
         mContext = context
     }
+
+    private fun makeHash(date: String) : String = date.split(" ")[0]
 }
