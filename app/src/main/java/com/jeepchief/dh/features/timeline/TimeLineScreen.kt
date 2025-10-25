67d4d3a44f4193b17cca121f08629f81a4553790
LeeJaeWon <ghkdwpqndtls@naver.com>
2025-10-25 16:04:10 +0900

    - [타임라인] > 아이템 획득 요약 기능 개발완료, 레이드/레기온 요약 기능 개발 70% 완료

--- app/src/main/java/com/jeepchief/dh/features/timeline/TimeLineScreen.kt
+++ app/src/main/java/com/jeepchief/dh/features/timeline/TimeLineScreen.kt
@@ -6,26 +6,26 @@ import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.jeepchief.dh.R
@@ -50,7 +51,6 @@ import com.jeepchief.dh.features.main.navigation.DhCircularProgress
 import com.jeepchief.dh.features.main.navigation.Divider
 import com.jeepchief.dh.features.main.navigation.ItemCard
 import com.jeepchief.dh.features.main.navigation.ItemInfoDialog
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -62,11 +62,18 @@ fun TimeLineScreen(
         val viewModel: DhMainViewModel = viewModel(LocalActivity.current as MainActivity)
         val timeLine by timeLineViewModel.timeLine.collectAsState()
         val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
-        val isShowingItemInfoDialog by stateViewModel.isShowingItemInfoDialog.collectAsState()
         val itemInfo by viewModel.itemInfo.collectAsState()
-        val isShowingNotFoundResult by remember { mutableStateOf(false) }
-        var isShowingSummaryDialog by remember { mutableStateOf(false) }
-        val itemSummaryList = remember { mutableListOf<ItemRows>() }
+        val itemSummary by timeLineViewModel.itemSummary.collectAsState()
+        val raidSummary by timeLineViewModel.raidSummary.collectAsState()
+
+        val itemSummaryMap = remember { mutableMapOf<String, MutableList<ItemRows>>() }
+        var clickedDate by remember { mutableStateOf("") }
+
+        val isShowingItemInfoDialog by stateViewModel.isShowingItemInfoDialog.collectAsState()
+        var isShowingNotFoundResult by remember { mutableStateOf(false) }
+        var isDailyShowingItemSummaryDialog by remember { mutableStateOf(false) }
+        var isShowingItemSummaryDialog by remember { mutableStateOf(false) }
+        var isShowingRaidSummaryDialog by remember { mutableStateOf(false) }
 
 
         LaunchedEffect(Unit) {
@@ -75,50 +82,72 @@ fun TimeLineScreen(
             }
         }
 
-        timeLine.timeline?.let {
-            Spacer(Modifier.height(5.dp))
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                val groupedDate = it.rows.groupBy { it.date.substring(0, 10) }
-                groupedDate.forEach { (date, rows) ->
-                    item(key = date) {
-                        Text(
-                            modifier = Modifier.clickable { isShowingSummaryDialog = true },
-                            text = date,
-                            color = Color.White,
-                            fontWeight = FontWeight.Bold,
-                            fontSize = TextUnit(20f, TextUnitType.Sp)
-                        )
-                        Spacer(Modifier.height(5.dp))
-                    }
-                    items(rows) { row ->
-                        row.data.also {
-                            if(it.itemId?.isNotEmpty() == true && it.itemName?.isNotEmpty() == true) {
-                                itemSummaryList.add(
-                                    ItemRows(
-                                        itemId = it.itemId ?: "",
-                                        itemName = it.itemName ?: "",
-                                        itemRarity = it.itemRarity ?: ""
+        LaunchedEffect(timeLine) {
+            timeLineViewModel.run {
+                getTotalSummary()
+                getRaidSummary()
+            }
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            HorizontalTabContainer(
+                "아이템 요약" to { isShowingItemSummaryDialog = true },
+                "레이드/레기온 요약" to { isShowingRaidSummaryDialog = true }
+            )
+
+            timeLine.timeline?.let {
+                Spacer(Modifier.height(5.dp))
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    val groupedDate = it.rows.groupBy { it.date.substring(0, 10) }
+                    groupedDate.forEach { (date, rows) ->
+                        val itemSummaryList = mutableListOf<ItemRows>()
+                        item(key = date) {
+                            Text(
+                                modifier = Modifier.clickable {
+                                    isDailyShowingItemSummaryDialog = true
+                                    clickedDate = date
+                                },
+                                text = date,
+                                color = Color.White,
+                                fontWeight = FontWeight.Bold,
+                                fontSize = TextUnit(20f, TextUnitType.Sp)
+                            )
+                            Spacer(Modifier.height(5.dp))
+                        }
+                        items(rows) { row ->
+                            row.data.also {
+                                if(it.itemId?.isNotEmpty() == true && it.itemName?.isNotEmpty() == true) {
+                                    itemSummaryList.add(
+                                        ItemRows(
+                                            itemId = it.itemId ?: "",
+                                            itemName = it.itemName ?: "",
+                                            itemRarity = it.itemRarity ?: ""
+                                        )
                                     )
-                                )
+                                }
                             }
+
+                            TimeLineCard(row) {
+                                row.data.itemId?.let { id ->
+                                    stateViewModel.setIsShowingItemInfoDialog(true)
+                                    viewModel.getItemInfo(id)
+                                }
+                            }
+                            Spacer(Modifier.height(5.dp))
                         }
 
-                        TimeLineCard(row) {
-                            row.data.itemId?.let { id ->
-                                stateViewModel.setIsShowingItemInfoDialog(true)
-                                viewModel.getItemInfo(id)
-                            }
-                        }
-                        Spacer(Modifier.height(5.dp))
+                        itemSummaryMap.put(date, itemSummaryList)
                     }
                 }
 
-            }
-
-        } ?: DhCircularProgress()
+            } ?: DhCircularProgress()
+        }
 
         if(isShowingItemInfoDialog) {
             ItemInfoDialog(itemInfo, stateViewModel)
@@ -127,55 +156,24 @@ fun TimeLineScreen(
         if(isShowingNotFoundResult) {
             Toast.makeText(LocalContext.current, LocalContext.current.getString(R.string.error_msg_not_found_timeline), Toast.LENGTH_SHORT).show()
             backDispatcher?.onBackPressed()
-            return@BaseScreen
         }
 
-        if(isShowingSummaryDialog) {
-            AlertDialog(
-                onDismissRequest = { isShowingSummaryDialog = false },
-                confirmButton = {
+        if(isDailyShowingItemSummaryDialog) {
+            DailyItemSummaryDialog(clickedDate, itemSummaryMap) {
+                isDailyShowingItemSummaryDialog = false
+            }
+        }
 
-                },
-                text = {
-                    LazyColumn {
-                        val taechoList = itemSummaryList.filter { it.itemRarity == "태초" }
-                        item {
+        if(isShowingItemSummaryDialog) {
+            ItemSummaryDialog(itemSummary) {
+                isShowingItemSummaryDialog = false
+            }
+        }
 
-                            Text(
-                                text = "태초 개수 : ${taechoList.size}",
-                                color = Color.White
-                            )
-                        }
-                        items(taechoList) {
-                            ItemCard(it)
-                        }
-
-                        val epicList = itemSummaryList.filter { it.itemRarity == "에픽" }
-                        item {
-                            Divider()
-                            Text(
-                                text = "에픽 개수 : ${epicList.size}",
-                                color = Color.White
-                            )
-                        }
-                        items(epicList) {
-                            ItemCard(it)
-                        }
-
-                        val legendaryList = itemSummaryList.filter { it.itemRarity == "레전더리" }
-                        item {
-                            Divider()
-                            Text(
-                                text = "레전더리 개수 : ${legendaryList.size}",
-                                color = Color.White
-                            )
-                        }
-                        items(legendaryList) {
-                            ItemCard(it)
-                        }
-                    }
-                }
-            )
+        if(isShowingRaidSummaryDialog) {
+            RaidSummaryDialog(raidSummary) {
+                isShowingRaidSummaryDialog = false
+            }
         }
     }
 }
@@ -212,6 +210,255 @@ fun TimeLineCard(row: TimeLineRows, isClickable: Boolean = false, clickCallback:
             )
         }
     }
+}
+
+@Composable
+fun ItemSummaryDialog(
+    itemSummaryMap: Map<String, List<ItemRows>>,
+    onDismiss: () -> Unit
+) {
+    var taechoCount = 0
+    var epicCount = 0
+    var legendaryCount = 0
+    var totalCount = 0
+
+    SideEffect {
+        var tempTCount = 0
+        var tempECount = 0
+        var tempLCount = 0
+        itemSummaryMap.forEach { (date, list) ->
+            tempTCount += list.filter { it.itemRarity == "태초" }.count()
+            tempECount += list.filter { it.itemRarity == "에픽" }.count()
+            tempLCount += list.filter { it.itemRarity == "레전더리" }.count()
+        }
+
+        taechoCount = tempTCount; epicCount = tempECount; legendaryCount = tempLCount
+        totalCount = taechoCount + epicCount + legendaryCount
+    }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false),
+        confirmButton = {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onDismiss
+            ) {
+                Text("닫기", color = Color.White)
+            }
+        },
+        text = {
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                item {
+                    Text(
+                        text = "총 획득 개수: ${totalCount}",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = """
+                            태초: $taechoCount
+                            에픽: $epicCount
+                            레전더리: $legendaryCount
+                        """.trimIndent(),
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Spacer(Modifier.height(5.dp))
+                    Divider()
+                }
+
+                itemSummaryMap.forEach { (date, list) ->
+                    item {
+                        Spacer(Modifier.height(10.dp))
+                        Text(
+                            text = date,
+                            color = Color.White
+                        )
+                    }
+                    items(list) {
+                        ItemCard(it)
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun DailyItemSummaryDialog(
+    clickedDate: String,
+    itemSummaryMap: Map<String, List<ItemRows>>,
+    onDismiss: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false),
+        confirmButton = {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onDismiss
+            ) {
+                Text("닫기", color = Color.White)
+            }
+        },
+        text = {
+            LazyColumn {
+                item {
+                    Text(
+                        text = "Date: $clickedDate",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = TextUnit(15f, TextUnitType.Sp)
+                    )
+                }
+
+                itemSummaryMap.get(clickedDate)?.filter { it.itemRarity == "태초" }?.let { taechoList ->
+                    if(taechoList.isEmpty()) return@let
+
+                    item { Spacer(Modifier.height(10.dp)) }
+                    items(taechoList) {
+                        ItemCard(it)
+                    }
+                    item {
+                        Text(
+                            text = "태초 개수 : ${taechoList.size}",
+                            color = Color.White
+                        )
+
+                        Spacer(Modifier.height(10.dp))
+                    }
+                }
+
+                itemSummaryMap.get(clickedDate)?.filter { it.itemRarity == "에픽" }?.let { epicList ->
+                    if(epicList.isEmpty()) return@let
+
+                    item { Spacer(Modifier.height(10.dp)) }
+                    items(epicList) {
+                        ItemCard(it)
+                    }
+                    item {
+                        Text(
+                            text = "에픽 개수 : ${epicList.size}",
+                            color = Color.White
+                        )
+                        Spacer(Modifier.height(10.dp))
+                    }
+                }
+
+                itemSummaryMap.get(clickedDate)?.filter { it.itemRarity == "레전더리" }?.let { legendaryList ->
+                    if(legendaryList.isEmpty()) return@let
+
+                    item { Spacer(Modifier.height(10.dp)) }
+                    items(legendaryList) {
+                        ItemCard(it)
+                    }
+                    item {
+                        Text(
+                            text = "레전더리 개수 : ${legendaryList.size}",
+                            color = Color.White
+                        )
+                        Spacer(Modifier.height(10.dp))
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun HorizontalTabContainer(vararg tabContent: Pair<String, () -> Unit>) {
+    Spacer(Modifier.height(5.dp))
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(tabContent) {
+            OutlinedButton(
+                onClick = it.second
+            ) {
+                Text(
+                    text = it.first,
+                    color = Color.White
+                )
+            }
+            Spacer(Modifier.width(5.dp))
+        }
+    }
+}
+
+@Composable
+fun RaidSummaryDialog(
+    raidSummaryMap: Map<String, List<String>>,
+    onDismiss: () -> Unit
+) {
+    var raidCount = 0
+    var regionCount = 0
+    var totalCount = 0
+
+    SideEffect {
+        var tempRaidCount = 0
+        var tempRegionCount = 0
+
+        raidSummaryMap.forEach { (date, list) ->
+            tempRaidCount += list.filter { it.contains("레이드") }.count()
+            tempRegionCount += list.filter { it.contains("레기온") }.count()
+        }
+        raidCount = tempRaidCount; regionCount = tempRegionCount
+        totalCount = raidCount + regionCount
+    }
+
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false),
+        confirmButton = {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = onDismiss
+            ) {
+                Text(text = "닫기", color = Color.White)
+            }
+        },
+        text = {
+            LazyColumn {
+                item {
+                    Text(
+                        text = "레이드/레기온 클리어 횟수: ${totalCount}회",
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = """
+                            레이드: ${raidCount}회
+                            레기온: ${regionCount}회
+                        """.trimIndent(),
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Divider()
+                }
+
+                raidSummaryMap.forEach { (date, list) ->
+                    item {
+                        Spacer(Modifier.height(10.dp))
+                        Text(
+                            text = date,
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    items(list) {
+                        Text(
+                            text = it,
+                            color = Color.White
+                        )
+                        Spacer(Modifier.height(10.dp))
+                    }
+                }
+            }
+        }
+    )
 }
 
 fun getTimeLineDesc(context: Context, row: TimeLineRows) : Map<String, String> {
--- app/src/main/java/com/jeepchief/dh/features/timeline/TimeLineViewModel.kt
+++ app/src/main/java/com/jeepchief/dh/features/timeline/TimeLineViewModel.kt
@@ -1,17 +1,23 @@
 package com.jeepchief.dh.features.timeline
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jeepchief.dh.R
+import com.jeepchief.dh.core.network.dto.ItemRows
 import com.jeepchief.dh.core.repository.DhApiRepository
 import com.jeepchief.dh.core.network.dto.TimeLineDTO
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class TimeLineViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val apiRepository: DhApiRepository
 ) : ViewModel() {
 
@@ -22,5 +28,64 @@ class TimeLineViewModel @Inject constructor(
         viewModelScope.launch {
             _timeLine.value = apiRepository.getTimeLine(serverId, characterId)
         }
+    }
+
+    private val _itemSummary = MutableStateFlow(HashMap<String, List<ItemRows>>())
+    val itemSummary = _itemSummary.asStateFlow()
+    fun getTotalSummary() {
+        val baseSummaryMap = _timeLine.value.timeline?.rows?.groupBy { it.date.substring(0, 10) } ?: return
+
+        val itemSummaryMap = mutableMapOf<String, List<ItemRows>>()
+
+        baseSummaryMap.forEach { (date, timeLineList) ->
+            val tempList = mutableListOf<ItemRows>()
+            timeLineList.filter { !it.data.itemId.isNullOrEmpty() }.forEach { timeLine ->
+                tempList.add(
+                    ItemRows(
+                        timeLine.data.itemId ?: "",
+                        timeLine.data.itemName ?: "",
+                        timeLine.data.itemRarity ?: ""
+                    )
+                )
+            }
+
+            itemSummaryMap.put(date, tempList)
+        }
+
+        _itemSummary.value = HashMap(itemSummaryMap.filter { it.value.isNotEmpty() }.toSortedMap())
+    }
+
+    private val _raidSummary  = MutableStateFlow(HashMap<String, List<String>>())
+    val raidSummary = _raidSummary.asStateFlow()
+    fun getRaidSummary() {
+        val baseSummaryMap = _timeLine.value.timeline?.rows?.groupBy { it.date.substring(0, 10) } ?: return
+
+        val raidSummaryMap = mutableMapOf<String, List<String>>()
+        baseSummaryMap.forEach { (date, timeLineList) ->
+            val tempList = mutableListOf<String>()
+            timeLineList.forEach { timeLine ->
+                when (timeLine.code) {
+                    201 -> {
+                        tempList.add(
+                            String.format(
+                                context.getString(R.string.timeline_clear_raid),
+                                "${timeLine.data.raidName} - ${timeLine.data.modeName}"
+                            )
+                        )
+                    }
+                    209 -> {
+                        tempList.add(
+                            String.format(
+                                context.getString(R.string.timeline_clear_region),
+                                timeLine.data.regionName
+                            )
+                        )
+                    }
+                }
+            }
+            raidSummaryMap.put(date, tempList)
+        }
+
+        _raidSummary.value = HashMap(raidSummaryMap.filter { it.value.isNotEmpty() }.toSortedMap())
     }
 }
